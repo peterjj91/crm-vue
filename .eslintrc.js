f63fde78683dbf52b7c738af8ae1d3eb61f6c7b1
Popeskul Pavlo <peterjj91@gmail.com>
2020-05-24 22:00:38 +0300

    Initial start

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,20 @@
 module.exports = {
   root: true,
+  parserOptions: {
+    parser: "babel-eslint",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
   env: {
-    node: true
+    node: true,
   },
   extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
-  parserOptions: {
-    parser: "babel-eslint"
-  },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-  }
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    eqeqeq: ["off"],
+    "no-new": ["off"],
+  },
 };
