daf1043d00eb303ab439067f9d4284c3bb087aaa
Popeskul Pavlo <peterjj91@gmail.com>
2020-05-26 15:53:09 +0300

    Add categry form

---
+++ src/store/category.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+
+export default {
+  actions: {
+    async createCategory({ dispatch, commit }, { title, limit }) {
+      try {
+        const uid = await dispatch("getUid");
+        const category = await firebase
+          .database()
+          .ref(`/users/${uid}/categories`)
+          .push({ title, limit });
+        return { title, limit, id: category.key };
+      } catch (error) {
+        commit("setError", error);
+        throw error;
+      }
+    }
+  }
+};
--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -5,66 +5,31 @@
     </div>
     <section>
       <div class="row">
-        <div class="col s12 m6">
-          <div>
-            <div class="page-subtitle">
-              <h4>Create</h4>
-            </div>
-
-            <form>
-              <div class="input-field">
-                <input id="name" type="text" />
-                <label for="name">Title</label>
-                <span class="helper-text invalid">Enter the title</span>
-              </div>
-
-              <div class="input-field">
-                <input id="limit" type="number" />
-                <label for="limit">Limit</label>
-                <span class="helper-text invalid">Minimum value</span>
-              </div>
-
-              <button class="btn waves-effect waves-light" type="submit">
-                Create
-                <i class="material-icons right">send</i>
-              </button>
-            </form>
-          </div>
-        </div>
-        <div class="col s12 m6">
-          <div>
-            <div class="page-subtitle">
-              <h4>Edit</h4>
-            </div>
-
-            <form>
-              <div class="input-field">
-                <select>
-                  <option>Category</option>
-                </select>
-                <label>Select a category</label>
-              </div>
-
-              <div class="input-field">
-                <input type="text" id="name" />
-                <label for="name">Title</label>
-                <span class="helper-text invalid">TITLE</span>
-              </div>
-
-              <div class="input-field">
-                <input id="limit" type="number" />
-                <label for="limit">Limit</label>
-                <span class="helper-text invalid">LIMIT</span>
-              </div>
-
-              <button class="btn waves-effect waves-light" type="submit">
-                Update
-                <i class="material-icons right">send</i>
-              </button>
-            </form>
-          </div>
-        </div>
+        <CategoryCreate @created="addNewCategory" />
+        <CategoryEdit />
       </div>
     </section>
   </div>
 </template>
+
+<script>
+import CategoryCreate from "@/components/CategoryCreate";
+import CategoryEdit from "@/components/CategoryEdit";
+
+export default {
+  name: "categories",
+  data: () => ({
+    categories: []
+  }),
+  methods: {
+    addNewCategory(category) {
+      console.log(this.categories);
+      this.categories.push(category);
+    }
+  },
+  components: {
+    CategoryCreate,
+    CategoryEdit
+  }
+};
+</script>
